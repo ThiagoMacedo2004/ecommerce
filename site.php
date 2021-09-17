9194dcd0b6730c3985643443cdf2bc3797141938
Thiago Macedo <tsouzamacedo@gmail.com>
2021-09-17 15:42:31 -0300

    site - produtos

--- site.php
+++ site.php
@@ -1,6 +1,7 @@
 <?php
 
 use Hcode\Model\Category;
+use Hcode\Model\Product;
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
@@ -9,8 +10,12 @@ use \Hcode\Model\User;
 
 $app->get('/', function() {
     
+	$products = Product::listAll();
+
 	$page = new Page();
-	$page->setTpl("index");
+	$page->setTpl("index", [
+		'products'=>Product::checkList($products)
+	]);
 	
 	
 });
